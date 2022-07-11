ca90f80a15dd20ae1ed4a32b7b104a2b8f1f4cbe
Edcleidson Junior <eddieprofessionalmail@gmail.com>
2022-07-11 00:58:53 -0300

    feature: add S3 Service

--- localstack/create-s3-bucket.sh
+++ localstack/create-s3-bucket.sh
@@ -6,10 +6,14 @@ awslocal s3api create-bucket --bucket planets-bucket --output table | cat
 
 
 # Add a file to bucket
+echo ""
 echo "Adding file helloworld.txt to the bucket:"
+
 touch helloworld.txt
 awslocal s3 cp helloworld.txt s3://planets-bucket/planets-helloworld
 
 #List files in bucket
+echo ""
 echo "Listing Bucket Files:"
-aws s3 ls s3://planets-bucket --recursive --human-readable --summarize
+
+awslocal s3 ls s3://planets-bucket --recursive --human-readable --summarize

98ea453b6347c52674b2df45d078db15a4035b32
edcleidson_junior <edcleidson_junior@sicredi.com.br>
2022-08-23 02:00:58 -0300

    chore: adjusting env vars to test

--- scripts/test-mode.sh
+++ scripts/test-mode.sh
@@ -3,25 +3,26 @@
 # Run compose to up environment
 chmod +x ./scripts/cache-docker/mount-environment.sh
 ./scripts/cache-docker/mount-environment.sh
+planet-delete.fifo
 
-export MONGO_HOST=localhost
-export MONGO_USER=admin
-export MONGO_PASSWORD=password
-export MONGO_PORT=27017
-export MONGO_DB=starwars
-export MONGO_CONTAINER_NAME=mongoservice
-export AWS_SECRET_KEY=SECRET
-export AWS_ACCESS_KEY=ACCESS
+export KAFKA_BOOTSTRAP_ADDRESS=http://localhost:9093
 export AWS_REGION=us-east-1
-export SNS_ENDPOINT=http://localhost:4566
-export SQS_ENDPOINT=http://localhost:4566
+export MONGO_AUTH_SOURCE=admin
+export MONGO_HOST=localhost
 export SQS_PLANET_DELETE_URL=http://localhost:4566/queue/planet-delete.fifo
 export SWAPI_URL=https://swapi.dev/api
+export MONGO_DB=starwars
+export SQS_ENDPOINT=http://localhost:4566
+export MONGO_USER=admin
 export ENV=ENV
-export API_PORT=8080
+export MONGO_CONTAINER_NAME=mongoservice
 export PROFILE=test
-export MONGO_AUTH_SOURCE=admin
-export KAFKA_BOOTSTRAP_ADDRESS=http://localhost:9093
+export SNS_ENDPOINT=http://localhost:4566
+export API_PORT=8080
+export MONGO_PORT=27017
+export MONGO_PASSWORD=password
+export AWS_SECRET_KEY=SECRET
+export AWS_ACCESS_KEY=ACCESS
 
 chmod +x gradlew
 ./gradlew test jacocoTestReport $1 $2
